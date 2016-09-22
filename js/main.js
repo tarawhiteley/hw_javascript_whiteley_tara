489726c58eb59343773eb2f0c56b2dba5c3295c3
Tara Whiteley <tarawhiteley@gmail.com>
2016-09-22 19:51:32 -0400

    add presidents object variable

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var presidents = [
+    {
+        firstName: 'Barack',
+        lastName: 'Obama',
+        termLength: 2,
+        party: 'Democratic',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'George W.',
+        lastName: 'Bush',
+        termLength: 2,
+        party: 'Republican',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'Bill',
+        lastName: 'Clinton',
+        termLength: 2,
+        party: 'Democratic',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'George H. W.',
+        lastName: 'Bush',
+        termLength: 1,
+        party: 'Republican',
+        yearsOfPresidency: 4
+    },
+
+    {
+        firstName: 'Ronald',
+        lastName: 'Reagan',
+        termLength: 2,
+        party: 'Republican',
+        yearsOfPresidency: 8
+    }
+];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
